c01e6fa790522bd77ba1be5083a6203b3b461f2f
Santiago Gutierrez <119289025+Santiago1721@users.noreply.github.com>
2023-02-01 12:10:15 +0100

    Create Ejercicio1.java

---
+++ Ejercicio1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Calculadora {
+ public static void main(String[] args) {
+ Scanner s= new Scanner(System.in);
+ 
+ System.out.print("indica el primer número; "); 
+ int a=s.nextInt();
+ System.out.print("indica el segundor número; "); 
+ int b=s.nextInt();
+ //invocamos los diferentes metodos
+ System.out.println("Suma: " + suma(a, b));
+ System.out.println("Resta: " + resta(a, b));
+ System.out.println("Producto: " + producto(a, b));
+ System.out.println("División: " + division(a, b));
+ }
+ // métodos static, no necesitan instanciar una clase.
+ public static int suma(int a, int b) {
+ return a + b;
+ }
+ public static int resta(int a, int b) {
+ return a - b;
+ }
+ public static int producto(int a, int b) {
+ return a * b;
+ }
+ public static int division(int a, int b) {
+ return a / b;
+ }
+}
